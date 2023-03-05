59d77b0d6c339435a756ede94f0a270106d0da63
saibhargav0701 <122075049+saibhargav0701@users.noreply.github.com>
2023-03-05 21:39:16 +0530

    Add files via upload
    starting with javascript

---
+++ start.js
@@ -0,0 +1,85 @@
+// let fname=10;
+// const mobile=20;
+// let z = fname + mobile
+
+// console.log(`Output for the above operation is ${z}`);
+
+// let x = 100;
+// let y = 200;
+
+// let sum = x + y;
+
+// console.log(sum);
+
+// let age = 11;
+// if(age>=18){
+//     console.log("You are eligible to vote");
+// }
+// else{
+//     console.log("You are minor and not eligible to vote");
+// }
+
+// switch(age){
+//     case 11:
+//         console.log("this is the number you are searching for",age);
+//         break;
+//     case 12:
+//         console.log("this is the number you are searching for",age);
+//         break;
+//     default:
+//         console.log("your number is not here");
+// }
+
+
+// let holiday = false;
+// holiday?console.log("it is holiday"):console.log("it is a working day");
+
+// const now = new Date();
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDay());
+// console.log(now.getTime());
+// console.log(now.getHours());
+// console.log(now.getMinutes());
+// console.log(now.getSeconds());
+
+// const PI = Math.PI;
+// console.log(PI);
+// console.log(Math.round(PI));
+// console.log(Math.floor(PI));
+// console.log(Math.ceil(PI));
+
+// console.log(Math.max(10,12,15,18));
+// console.log(Math.min(10,12,15,18));
+// console.log(Math.random()*100);
+// console.log(Math.pow(10,2));
+// console.log(Math.sin(90));
+
+LOOPS
+let balance = 10;
+do{
+    console.log("Value of balance is",balance);
+    balance++;
+}
+while(balance <=20);
+
+while(balance<100){
+    console.log("Value is ",balance);
+    balance++;
+}
+
+for(let balance =0;balance<=20;balance++){
+    console.log("value:",balance);
+}
+
+
+
+
+
+
+
+
+
+
+
+
